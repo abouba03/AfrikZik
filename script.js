f90df93198be445680c91c4ac506cbfed41dfed9
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2022-11-25 01:46:04 +0300

    update animations

--- script.js
+++ script.js
@@ -110,6 +110,16 @@ videos(
     "landing-img",
     "play",
     "Some quick example text to build on the card title and make up the bulk of the card's content."
+) +
+videos(
+    "landing-img",
+    "play",
+    "Some quick example text to build on the card title and make up the bulk of the card's content."
+) +
+videos(
+    "landing-img",
+    "play",
+    "Some quick example text to build on the card title and make up the bulk of the card's content."
 )
 
 
